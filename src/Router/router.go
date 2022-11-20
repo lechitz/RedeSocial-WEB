f325821637d79c6e1127ef007664d2813e5b3eb9
Felipe Bravo <felipe.lechitz@gmail.com>
2022-11-20 18:04:20 -0300

    Configurando rotas iniciais

--- src/Router/router.go
+++ src/Router/router.go
@@ -1,8 +1,12 @@
 package Router
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"webapp/src/Router/rotas"
+)
 
 //Gerar retorna um router com todas as rotas configuradas
 func Gerar() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return rotas.Configurar(r)
 }
