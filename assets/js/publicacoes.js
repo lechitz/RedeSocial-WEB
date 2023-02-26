1aa9af818fe5ef06096fbe902f24d2faf869d291
Felipe Bravo <felipe.lechitz@gmail.com>
2023-02-26 17:56:16 -0300

    Atualização de publicação

--- assets/js/publicacoes.js
+++ assets/js/publicacoes.js
@@ -1,7 +1,9 @@
 $('#nova-publicacao').on('submit', criarPublicacao);
+
 $(document).on('click', '.curtir-publicacao', curtirPublicacao);
 $(document).on('click', '.descurtir-publicacao', descurtirPublicacao);
-// $('.curtir-publicacao').on('click', curtirPublicacao);
+
+$('#atualizar-publicacao').on('click', atualizarPublicacao);
 
 function criarPublicacao(evento) {
     evento.preventDefault();
@@ -74,4 +76,25 @@ function descurtirPublicacao(evento) {
     }).always(function () {
         elementoClicado.prop('disabled', false);
     });
+}
+
+function atualizarPublicacao(evento) {
+    $(this).prop('disabled', true);
+
+    const publicacaoId = $(this).data('publicacao-id');
+
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}`,
+        method: "PUT",
+        data: {
+            titulo: $('#titulo').val(),
+            conteudo: $('#conteudo').val()
+        }
+    }).done(function () {
+        alert("Publicação editada com sucesso!");
+    }).fail(function () {
+        alert("Erro ao editar publicação.");
+    }).always(function () {
+        $('#atualizar-publicacao').prop('disabled', false);
+    })
 }
